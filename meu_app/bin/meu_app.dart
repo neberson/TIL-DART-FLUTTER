f789b22659d49792ecb671d41fa02cd76133feba
Neberson Andrade <neberson_mtv@hotmail.com>
2023-08-30 21:20:45 -0300

    Realizado a implementação de exemplos de loops em dart.

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,10 +1,12 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
-import 'package:meu_app/Condicionais/if_calculadora.dart' as app_if_calculadora;
-import 'package:meu_app/condicionais/if_nota.dart' as app_if_nota;
-import 'package:meu_app/condicionais/switch_case.dart' as app_switch_case;
+import 'package:meu_app/Loops/for.dart' as app_for;
+import 'package:meu_app/Loops/while.dart' as app_while;
+import 'package:meu_app/Loops/for_each.dart' as app_for_each;
+import 'package:meu_app/Loops/do_while.dart' as app_do_while;
 
 void main(List<String> arguments) {
-  //app_if_calculadora.main(arguments);
-  //app_if_nota.main(arguments);
-  app_switch_case.main(arguments);
+  //app_for.main(arguments);
+  //app_while.main(arguments);
+  //app_for_each.main(arguments);
+  app_do_while.main(arguments);
 }
