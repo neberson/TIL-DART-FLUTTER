afda908157b20373add74007e3839f4f786da3c3
Neberson Andrade <neberson_mtv@hotmail.com>
2023-08-30 21:12:44 -0300

    Realizado a implementação de exemplos para as condicionais do dart

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,22 +1,10 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
-import 'package:meu_app/TiposDados/bool.dart' as app_bool;
-import 'package:meu_app/TiposDados/const.dart' as app_const;
-import 'package:meu_app/TiposDados/date.dart' as app_date;
-import 'package:meu_app/TiposDados/double.dart' as app_double;
-import 'package:meu_app/TiposDados/dynamic.dart' as app_dynamic;
-import 'package:meu_app/TiposDados/int.dart' as app_int;
-import 'package:meu_app/TiposDados/list.dart' as app_list;
-import 'package:meu_app/TiposDados/map.dart' as app_map;
-import 'package:meu_app/TiposDados/string.dart' as app_string;
+import 'package:meu_app/Condicionais/if_calculadora.dart' as app_if_calculadora;
+import 'package:meu_app/condicionais/if_nota.dart' as app_if_nota;
+import 'package:meu_app/condicionais/switch_case.dart' as app_switch_case;
 
 void main(List<String> arguments) {
-  //app_bool.main(arguments);
-  //app_const.main(arguments);
-  //app_date.main(arguments);
-  //app_double.main(arguments);
-  //app_dynamic.main(arguments);
-  //app_int.main(arguments);
-  //app_list.main(arguments);
-  //app_map.main(arguments);
-  app_string.main(arguments);
+  //app_if_calculadora.main(arguments);
+  //app_if_nota.main(arguments);
+  app_switch_case.main(arguments);
 }
