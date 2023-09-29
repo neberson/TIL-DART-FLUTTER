d2d2490b02d4efb2d9fc49e230478713f406ef75
Neberson Andrade <neberson_mtv@hotmail.com>
2023-09-29 11:16:42 -0300

    Realizado a implementação do widget Hero para animação de transição entre telas.

---
+++ trilhaapp_login_dio/lib/pages/card_detail_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:trilhaapp/model/card_detail.dart';
+
+class CardDetailPage extends StatelessWidget {
+  final CardDetail cardDetail;
+  const CardDetailPage({super.key, required this.cardDetail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: cardDetail.id,
+      transitionOnUserGestures: true,
+      child: SafeArea(
+        child: Scaffold(
+          //appBar: AppBar(),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.close)),
+                Center(
+                  child: Image.network(
+                    cardDetail.url,
+                    height: 100,
+                  ),
+                ),
+                Text(
+                  cardDetail.title,
+                  style: const TextStyle(
+                      fontSize: 26, fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: Text(
+                    textAlign: TextAlign.justify,
+                    cardDetail.text,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- trilhaapp_login_dio/lib/pages/card_page.dart
+++ trilhaapp_login_dio/lib/pages/card_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/model/card_detail.dart';
+import 'package:trilhaapp/pages/card_detail_page.dart';
 
 class CardPage extends StatefulWidget {
   const CardPage({super.key});
@@ -8,6 +10,11 @@ class CardPage extends StatefulWidget {
 }
 
 class _CardPageState extends State<CardPage> {
+  CardDetail cardDetail = CardDetail(
+      1,
+      "Meu card",
+      "https://hermes.digitalinnovation.one/assets/diome/logo.png",
+      "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.");
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,46 +22,60 @@ class _CardPageState extends State<CardPage> {
         Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           width: double.infinity,
-          child: Card(
-            elevation: 5,
-            shadowColor: const Color.fromARGB(255, 135, 67, 186),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
+          child: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => CardDetailPage(
+                            cardDetail: cardDetail,
+                          )));
+            },
+            child: Hero(
+              tag: cardDetail.id,
+              child: Card(
+                elevation: 5,
+                shadowColor: const Color.fromARGB(255, 135, 67, 186),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Image.network(
-                        "https://hermes.digitalinnovation.one/assets/diome/logo.png",
-                        width: 40,
+                      Row(
+                        children: [
+                          Image.network(
+                            cardDetail.url,
+                            width: 40,
+                          ),
+                          Text(
+                            cardDetail.title,
+                            style: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.w700),
+                          ),
+                        ],
                       ),
-                      const Text(
-                        "Meu card",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w700),
+                      const SizedBox(
+                        height: 10,
                       ),
+                      Text(
+                        textAlign: TextAlign.justify,
+                        cardDetail.text,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      Container(
+                          width: double.infinity,
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Ler mais",
+                                style: TextStyle(
+                                    decoration: TextDecoration.underline),
+                              )))
                     ],
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    textAlign: TextAlign.justify,
-                    "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Container(
-                      width: double.infinity,
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            "Ler mais",
-                            style:
-                                TextStyle(decoration: TextDecoration.underline),
-                          )))
-                ],
+                ),
               ),
             ),
           ),
