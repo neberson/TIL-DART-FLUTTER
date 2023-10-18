c4dd333a0b3dc6fb745780ab78f1960de4c16a68
Neberson Andrade <neberson_mtv@hotmail.com>
2023-10-18 11:55:30 -0300

    Realizado a alteração do icone do aplicativa utilizando o pacote flutter_launcher_icons

--- trilhaapp_login_dio/lib/repositories/back4app/back4app_custom_dio.dart
+++ trilhaapp_login_dio/lib/repositories/back4app/back4app_custom_dio.dart
@@ -5,6 +5,11 @@ class Back4AppCustomDio {
   final Dio _dio = Dio();
 
   Back4AppCustomDio() {
+    _dio.options.headers["X-Parse-Application-Id"] =
+        dotenv.get('BACK4APPPLICATIONID');
+
+    _dio.options.headers["X-Parse-REST-API-Key"] =
+        dotenv.get('BACK4APPPLICATIONRESTAPIKEY');
     _dio.options.headers["Content-Type"] = "application/json";
     _dio.options.baseUrl = dotenv.get('BACK4APPPLICATIONBASEURL');
   }
